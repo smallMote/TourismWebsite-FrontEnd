9cc66ecd5589c423a2a877e5b3d2998efc9e43fd
smallMote <1746671849@qq.com>
2018-06-08 13:52:41 +0800

    设置了header的轮播

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
             }
         }
     })();
-    console.log(class_nav.action.winResize);
+    // console.log(class_nav.action.winResize);
     /**
      * banner类
      * @type {{bean, action}}
@@ -34,6 +34,9 @@ $(document).ready(function () {
             action : {
                 nextImg : next,
                 prevImg : prev,
+                autoPlay : startPlay,
+                actionListHover : actionListHover,
+                containerHover : containerHover
             }
         }
     })();
@@ -45,9 +48,13 @@ $(document).ready(function () {
         //执行导航相关功能
         class_nav.action.winResize();
         class_nav.action.menuBtn();
+        //执行header的轮播
+        class_banner.action.autoPlay();
+        class_banner.action.actionListHover();
+        class_banner.action.containerHover();
     }
 
-    //初始化成员变量以及属性设置
+    //初始导航栏相关的化成员变量以及属性设置
     function init_nav() {
         return (function () {
             const menuBtn = $('header nav section .menu-btn a'),
@@ -57,12 +64,11 @@ $(document).ready(function () {
                 menuBtn : menuBtn,
                 menuList : menuList,
                 proprty : {
-                    clickFlag : true//默认单击
+                    clickFlag : true,//默认单击,
                 }
             }
         })();
     }
-    let clickFlag = true;//默认单击
     //窗口变化监听
     function winResize(){
         let menuList = class_nav.bean.domNode.menuList;
@@ -97,30 +103,41 @@ $(document).ready(function () {
             class_nav.bean.proprty.clickFlag = clickFlag;//回溯
         });
     }
-    //初始化变量
+    //初始化变量banner相关的成员变量以及相关属性
     function init_banner(){
         //为了保证class_banner为最上文，使用开放式的function+闭包
         return  (function () {
+            //第一个大轮播
+            let timer;
+            let $container = $('header .banner-header .container');
+            let $imgList = $container.find('*');
+            let $actionList = $('header .banner-header .action').find('*');
+            //热门新闻轮播
             let $imgContainer = $('.hot .img-container');
             let $img = $imgContainer.find('*');
-            $imgContainer.height($img.height());//设置最外层banner的高度
+                $imgContainer.height($img.height());//设置最外层banner的高度
             let $banner = $('.hot .banner');
             let $actionLeft = $('.hot .action-left');
             let $actionRight = $('.hot .action-right');
             let imgWidth = $img.width();
             let animateTime = $img.css('transition-duration');//动画时间
-            animateTime = parseInt(animateTime) * 1000;//图片动画时间转换成ms
-            $imgContainer.width($img.length * imgWidth);//设置图片容器的容器宽度，有图片数量和本省的宽度决定
-            $banner.width($img.width() * 3);//设置显示图片容器的宽度
+                animateTime = parseInt(animateTime) * 1000;//图片动画时间转换成ms
+                $imgContainer.width($img.length * imgWidth);//设置图片容器的容器宽度，有图片数量和本省的宽度决定
+                $banner.width($img.width() * 3);//设置显示图片容器的宽度
             return {
                 $imgContainer : $imgContainer,
                 $img : $img,
                 $banner : $banner,
                 $actionLeft : $actionLeft,
                 $actionRight : $actionRight,
+                $container : $container,
+                $imgList : $imgList,
+                $actionList : $actionList,
                 proprty : {
                     imgWidth : imgWidth,
-                    animateTime : animateTime
+                    animateTime : animateTime,
+                    count : 0,
+                    timer :timer,//外部设置
                 }
             }
         })();
@@ -201,4 +218,47 @@ $(document).ready(function () {
             }
         }
     })();
+    //第一个大轮播
+    //自动播放
+    function startPlay(){
+        let imgList = class_banner.bean.domNode.$imgList;
+        let actionList = class_banner.bean.domNode.$actionList;
+        let count = class_banner.bean.proprty.count;
+        class_banner.bean.proprty.timer = setInterval(function () {
+            imgList.not(imgList[count]).fadeOut(500);
+            $(imgList[count]).fadeIn(500);
+            actionList.css({background : 'none',opacity : '.8'});
+            $(actionList[count]).css({backgroundColor : 'white' , opacity : 1});
+            count++;
+            if (count === imgList.length){
+                count = 0;
+            }
+            class_banner.bean.proprty.count = count;
+        },2000);
+    }
+    //控制器控制轮播
+    function actionListHover(){
+        let actionList = class_banner.bean.domNode.$actionList;
+        let imgList = class_banner.bean.domNode.$imgList;
+        actionList.hover(function () {
+            clearInterval(class_banner.bean.proprty.timer);
+            let evRoot = $(this);
+            let rootIndex = evRoot.index();
+            imgList.not(imgList[rootIndex]).fadeOut(500);
+            $(imgList[rootIndex]).fadeIn(500);
+            count = rootIndex;
+        },function () {
+            startPlay();
+        });
+    }
+    //整体面板控制是否自动轮播
+    function containerHover(){
+        let container = class_banner.bean.domNode.$container;
+        container.hover(function () {
+            clearInterval(class_banner.bean.proprty.timer);
+            container.css({'cursor': 'pointer'});
+        },function () {
+            startPlay();
+        });
+    }
 });
